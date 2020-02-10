0b51c8a4de7111eeef69d77ea736d0ba95649c71
venkatesh-1234 <53892301+venkatesh-1234@users.noreply.github.com>
2020-02-10 15:06:09 +0530

    Update groups.js

--- groups.js
+++ groups.js
@@ -5,8 +5,8 @@ describe(' test cases for groups', function () {
         req.query('/groups',null,'rtpq20xndjc', function (err, res, body) {
             var output = res.statusCode;
             var data = JSON.parse(res.body);
-			console.log(data)
-			assert.equal(data.success,true)
+	    console.log(data)
+	    assert.equal(data.success,true)
             assert.equal(output, 200)
             done();
         })
@@ -15,8 +15,8 @@ describe(' test cases for groups', function () {
         req.query('/groups',null,'rtpq20xndj', function (err, res, body) {
             var output = res.statusCode;
             var data = JSON.parse(res.body);
-			console.log(data)
-			assert.equal(data.success,false)
+	    console.log(data)
+	    assert.equal(data.success,false)
             assert.equal(output, 401)
             done();
         })
@@ -25,8 +25,8 @@ describe(' test cases for groups', function () {
         req.query('/groups',null,'', function (err, res, body) {
             var output = res.statusCode;
             var data = JSON.parse(res.body);
-			console.log(data)
-			assert.equal(data.success,false)
+	    console.log(data)
+	    assert.equal(data.success,false)
             assert.equal(output, 401)
             done();
         })
